91cd0f76eb481fcf8cd4dcce81fbce8ae8af6fbe
Sanket Jagtap <san7ketjagtap@gmail.com>
2021-07-07 16:50:11 +0530

    Broke down to check status of game into separate function Added unit tests to functions

--- snakes_ladder.py
+++ snakes_ladder.py
@@ -1,53 +1,59 @@
 import random
 
 
-class SnakesLadder:
+class snakesladder:
     """Snakes and ladder class"""
 
     def __init__(self, snakes, ladder):
         """Initilize the game with snakes and ladder mapping"""
-
-        player_name = input("***Enter your name***\n")
-        crooked = input("Run with a crooked dice. Y/N\n")
-        if crooked == 'Y':
+        self.snakes = snakes
+        self.ladder = ladder
+        self.player_name = input("***Enter your name***\n")
+        self.crooked = input("Run with a crooked dice. Y/N\n") or False
+        if self.crooked == "Y":
             crooked = True
-        print("Welcome to Snakes And Ladders {0}".format(player_name))
-        self.run(crooked, snakes, ladder)
+        print("Welcome to Snakes And Ladders {0}".format(self.player_name))
+        self.run(self.crooked)
 
     def dice_roll(self, crooked):
         """Returns the rolled dice. Crooked or normal
 
         :return int roll_dice : Face of currently rolled dice.
         """
-        if crooked:
+        if crooked is True:
             roll_dice = random.choice([2, 4, 6])
         else:
             roll_dice = random.randint(1, 6)
         return roll_dice
 
-    def check_snake(self, pos, snakes):
+    def check_snake(self, pos):
         """Checks if the current position have a snake and returns to tail
 
         :return int pos : if snake is detected, returns the tail position or remains the same
         """
-        if pos in snakes.keys():
-            pos = snakes.get(pos)
+        if pos in self.snakes.keys():
+            pos = self.snakes.get(pos)
             print("Looks like we hit a snake uhhh...Moving to tail")
             print("Current Position:{0}".format(pos))
         return pos
 
-    def check_ladder(self, pos, ladder):
+    def check_ladder(self, pos):
         """Checks if the current position have a snake and returns to tail
 
         :return int pos : if ladder is detected, returns the top position or remains the same
         """
-        if pos in ladder.keys():
-            pos = ladder.get(pos)
+        if pos in self.ladder.keys():
+            pos = self.ladder.get(pos)
             print("I hate stairs, but it's a shortcut. Taking a shortcut..")
             print("Current Position:{0}".format(pos))
         return pos
 
-    def run(self, crooked, snakes, ladder):
+    def check_status(self, pos, turn):
+        if pos >= 100:
+            print(f"You win on {turn} turns. Congratulations")
+            return True
+
+    def run(self, crooked):
         """Contains the core game logic and the turns the players plays until loss or win
 
         :param crooked: if set, returns only even numbers on dice roll
@@ -59,30 +65,16 @@ class SnakesLadder:
             print("Excellent roll, moving by {0} position".format(dice))
             pos = pos + dice
             print("Current Position:{0}".format(pos))
-            final_pos = self.check_ladder(pos, ladder)
-            final_pos = self.check_snake(pos, ladder)
+            final_pos = self.check_ladder(pos)
+            final_pos = self.check_snake(pos)
             pos = final_pos
             print("\n")
-            if pos >= 100:
-                print(f"You win on {i} turns. Congratulations")
-                exit(1)
-        print("You Lose")
+            self.check_status(pos, i)
+        return pos
 
 
-if __name__ == '__main__':
-    snakes = {
-        14: 7,
-        23: 17,
-        45: 5,
-        51: 33,
-        99: 24
-    }
+if __name__ == "__main__":
+    snakes = {14: 7, 23: 17, 45: 5, 51: 33, 99: 24}
 
-    ladder = {
-        8: 29,
-        22: 61,
-        54: 68,
-        66: 97,
-        72: 93
-    }
-    snakesladder = SnakesLadder(snakes, ladder)
+    ladder = {8: 29, 22: 61, 54: 68, 66: 97, 72: 93}
+    snakesladder = snakesladder(snakes, ladder)
