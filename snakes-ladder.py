18aba97775af4b2f663124d8cd92a84c38a7b488
Sanket Jagtap <san7ketjagtap@gmail.com>
2021-07-07 11:36:10 +0530

    Added Class definations and class methods. Implemented OOPs

--- snakes-ladder.py
+++ snakes-ladder.py
@@ -1,93 +1,88 @@
 import random
 
-snakes = {
-    14: 7,
-    23: 17,
-    45: 5,
-    51: 33,
-    99: 24
-}
 
-ladder = {
-    8: 29,
-    22: 61,
-    54: 68,
-    66: 97,
-    72: 93
-}
+class SnakesLadder:
+    """Snakes and ladder class"""
 
+    def __init__(self, snakes, ladder):
+        """Initilize the game with snakes and ladder mapping"""
 
-def setup_game():
-    """Setting up various variables in game.
+        player_name = input("***Enter your name***\n")
+        crooked = input("Run with a crooked dice. Y/N\n")
+        if crooked == 'Y':
+            crooked = True
+        print("Welcome to Snakes And Ladders {0}".format(player_name))
+        self.run(crooked, snakes, ladder)
 
-    :returns str player_name : Name of the player
-    """
-    player_name = input("***Enter your name***\n")
-    crooked = input("Run with a crooked dice. Y/N\n")
-    if crooked == 'Y':
-        crooked = True
-    print("Welcome to Snakes And Ladders {0}".format(player_name))
-    run(crooked)
-    return player_name
+    def dice_roll(self, crooked):
+        """Returns the rolled dice. Crooked or normal
 
+        :return int roll_dice : Face of currently rolled dice.
+        """
+        if crooked:
+            roll_dice = random.choice([2, 4, 6])
+        else:
+            roll_dice = random.randint(1, 6)
+        return roll_dice
 
-def dice_roll(crooked=True):
-    """Returns the rolled dice. Crooked or normal
+    def check_snake(self, pos, snakes):
+        """Checks if the current position have a snake and returns to tail
 
-    :return int roll_dice : Face of currently rolled dice.
-    """
-    if crooked:
-        roll_dice = random.choice([2, 4, 6])
-    else:
-        roll_dice = random.randint(1, 6)
-    return roll_dice
+        :return int pos : if snake is detected, returns the tail position or remains the same
+        """
+        if pos in snakes.keys():
+            pos = snakes.get(pos)
+            print("Looks like we hit a snake uhhh...Moving to tail")
+            print("Current Position:{0}".format(pos))
+        return pos
 
+    def check_ladder(self, pos, ladder):
+        """Checks if the current position have a snake and returns to tail
 
-def check_snake(pos):
-    """Checks if the current position have a snake and returns to tail
+        :return int pos : if ladder is detected, returns the top position or remains the same
+        """
+        if pos in ladder.keys():
+            pos = ladder.get(pos)
+            print("I hate stairs, but it's a shortcut. Taking a shortcut..")
+            print("Current Position:{0}".format(pos))
+        return pos
 
-    :return int pos : if snake is detected, returns the tail position or remains the same
-    """
-    if pos in snakes.keys():
-        pos = snakes.get(pos)
-        print("Looks like we hit a snake uhhh...Moving to tail")
-        print("Current Position:{0}".format(pos))
-    return pos
+    def run(self, crooked, snakes, ladder):
+        """Contains the core game logic and the turns the players plays until loss or win
 
-
-def check_ladder(pos):
-    """Checks if the current position have a snake and returns to tail
-
-    :return int pos : if ladder is detected, returns the top position or remains the same
-    """
-    if pos in ladder.keys():
-        pos = ladder.get(pos)
-        print("I hate stairs, but it's a shortcut. Taking a shortcut..")
-        print("Current Position:{0}".format(pos))
-    return pos
-
-
-def run(crooked):
-    """Contains the core game logic and the turns the players plays until loss or win
-
-    :param crooked: if set, returns only even numbers on dice roll
-    """
-    pos = 1
-    for i in range(1, 10):
-        print(f"Current turn {i}")
-        dice = dice_roll(crooked)
-        print("Excellent roll, moving by {0} position".format(dice))
-        pos = pos + dice
-        print("Current Position:{0}".format(pos))
-        final_pos = check_ladder(pos)
-        final_pos = check_snake(pos)
-        pos = final_pos
-        print("\n")
-        if pos >= 100:
-            print(f"You win on {i} turns. Congratulations")
-            exit(1)
-    print("You Lose")
+        :param crooked: if set, returns only even numbers on dice roll
+        """
+        pos = 1
+        for i in range(1, 10):
+            print(f"Current turn {i}")
+            dice = self.dice_roll(crooked)
+            print("Excellent roll, moving by {0} position".format(dice))
+            pos = pos + dice
+            print("Current Position:{0}".format(pos))
+            final_pos = self.check_ladder(pos, ladder)
+            final_pos = self.check_snake(pos, ladder)
+            pos = final_pos
+            print("\n")
+            if pos >= 100:
+                print(f"You win on {i} turns. Congratulations")
+                exit(1)
+        print("You Lose")
 
 
 if __name__ == '__main__':
-    setup_game()
+    snakes = {
+        14: 7,
+        23: 17,
+        45: 5,
+        51: 33,
+        99: 24
+    }
+
+    ladder = {
+        8: 29,
+        22: 61,
+        54: 68,
+        66: 97,
+        72: 93
+    }
+    snakesladder = SnakesLadder(snakes, ladder)
